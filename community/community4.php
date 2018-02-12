2c1d58e2314529e5025a414b2b8133eed55409c1
Seogeurim <geulims@naver.com>
2018-02-12 21:57:22 +0900

    add community file

---
+++ community/community4.php
@@ -0,0 +1,68 @@
+<!-- 프로젝트 구인 -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>프로젝트 구인</title>
+  </head>
+  <body>
+    <h1>ALPA 프로젝트 구인</h1>
+
+    <?php
+    $name = "post";
+
+    try{
+      $query = "select * from post where field = 3";
+      $db = new PDO("mysql:dbname=$name", "root","root");
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $rows = $db->query($query);
+
+      global $size, $post_num, $post_writer, $post_title, $post_date, $post_time, $post_hits;
+      $size=0;
+      foreach ($rows as $row) {
+        $size ++;
+        $post_num[] = $row['num'];
+        $post_writer[] = $row['writer'];
+        $post_title[] = $row['title'];
+        $post_date[] = $row['date'];
+        $post_time[] = $row['time'];
+        $post_hits[] = $row['hits'];
+      }
+
+    }catch(PDOException $ex){
+      echo "detail :".$ex->getMessage();
+    }
+
+    ?>
+
+    <div class="community">
+      <table>
+        <h2>게시판 명</h2>
+        <tr>
+          <th>글번호</th>
+          <th>작성자</th>
+          <th>제목</th>
+          <th>게시날짜</th>
+          <th>게시시간</th>
+          <th>조회수</th>
+        </tr>
+
+        <?php
+          for($i=0; $i<$size; $i++){
+            ?>
+            <tr>
+              <th><?=$post_num[$i]?></th>
+              <th><?=$post_writer[$i]?></th>
+              <th><?=$post_title[$i]?></th>
+              <th><?=$post_date[$i]?></th>
+              <th><?=$post_time[$i]?></th>
+              <th><?=$post_hits[$i]?></th>
+            </tr>
+            <?php
+          }
+        ?>
+      </table>
+    </div>
+
+  </body>
+</html>
