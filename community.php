e1e24c5cd091588dfb2ecbe3a8a6fbaa22052159
jisung0920@gmail.com <jisung0920@gmail.com>
2018-02-15 21:38:26 +0900

    css,login

--- community.php
+++ community.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="ko-KR">
 <head>
-
+	<meta charset="utf-8">
 	<title>Application Promgramming Association</title>
 	<link rel="shortcut icon" href="/css/img/favicon.ico"/>
-	<meta charset="utf-8">
 	<meta name="description" content="The ALPA website's main pasge">
 	<meta name="author" content="alpa20161104@gmail.com">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -26,81 +25,86 @@
 	<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
+
+	<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.3.0/prototype.js" type="text/javascript"></script>
+	<script src="community.js" type="text/javascript"></script>
 </head>
 <body>
 
 	<header id="name">
 
 		<img id="headImg" src="/css/img/headImg.png"> <!-- 아무거나 넣었음 -->
-		<h1><a id="Logo" href="index.html">ALPA</a></h1>
+		<h1><a id="Logo" href="index2.html">ALPA</a></h1>
 
 	</header>
 
 	<div id="menu2">
 		<div class="totalmenu">
-			<nav id="topMenu">
-				<ul>
-					<li class="topMenuLi">
-						<a  class="menuLink">About us</a>
-						<ul class="submenu">
-							<li><a href="introduction.html" class="submenuLink">Introduction</a></li>
-							<li><a href="organization.html" class="submenuLink">Organization Chart</a></li>
-							<li><a href="introproject.html" class="submenuLink">Project</a></li>
-							<li><a href="https://goo.gl/forms/jqoQ3rOTxwnHBld73" class="submenuLink">Apply</a></li>
-						</ul>
-					</li>
-					<li>|</li>
-					<li class="topMenuLi">
-						<a  class="menuLink">Notice</a>
-						<ul class="submenu">
-							<li><a href="notice.html" class="submenuLink">Notice</a></li>
-							<li><a href="https://drive.google.com/drive/folders/0BwLt-eXTUGwAbmpmZ1BmekV6RTg" class="submenuLink">Accounting</a></li>
-							<li><a href="https://drive.google.com/drive/folders/0BwLt-eXTUGwAemRoTjdKWmNhNTQ" class="submenuLink">Items</a></li>
+		<nav id="topMenu">
+			<ul>
+				<li class="topMenuLi">
+					<a  class="menuLink">About us</a>
+					<ul class="submenu">
+						<li><a href="introduction.html" class="submenuLink">Introduction</a></li>
+						<li><a href="organization.html" class="submenuLink">Organization Chart</a></li>
+						<li><a href="introproject.html" class="submenuLink">IntroProject</a></li>
+						<li><a href="apply.html" class="submenuLink">Apply</a></li>
+					</ul>
+				</li>
+				<li>|</li>
+				<li class="topMenuLi">
+					<a  class="menuLink">Notice</a>
+					<ul class="submenu">
+						<li><a href="notice.html" class="submenuLink">Notice</a></li>
+						<li><a href="capital.html" class="submenuLink">Capital</a></li>
+						<li><a href="supplies.html" class="submenuLink">Supplies</a></li>
 
-						</ul>
-					</li>
-					<li>|</li>
-					<li class="topMenuLi">
-						<a  class="menuLink">Reference</a>
-						<ul class="submenu">
-							<li><a href="project.html" class="submenuLink">Project</a></li>
-							<li><a href="seminar.html" class="submenuLink">Seminar</a></li>
-							<li><a href="https://drive.google.com/drive/folders/0Bx-YhTmZR8KeVUZrT1NBUkZzdmM" class="submenuLink">Shared Folder</a></li>
-						</ul>
-					</li>
-					<li>|</li>
-					<li class="topMenuLi">
-						<a  class="menuLink">Community</a>
-						<ul class="submenu">
-							<li><a href="community.php?field=0" class="submenuLink">Anonymous</a></li>
-							<li><a href="community.php?field=1" class="submenuLink">Suggestion</a></li>
-							<li><a href="community.php?field=2" class="submenuLink">Class Tip</a></li>
-							<li><a href="community.php?field=3" class="submenuLink">Recruitment</a></li>
-						</ul>
-					</li>
-					<li>|</li>
-					<li class="topMenuLi">
-						<a href="gallery.html" class="menuLink">Gallery</a>
-					</li>
-				</ul>
-			</nav>
-		<?php $field_num = $_GET["field"]; ?>
+					</ul>
+				</li>
+				<li>|</li>
+				<li class="topMenuLi">
+					<a  class="menuLink">Reference</a>
+					<ul class="submenu">
+						<li><a href="project.html" class="submenuLink">Project</a></li>
+						<li><a href="seminar.html" class="submenuLink">Seminar</a></li>
+						<li><a href="sharedfolder.html" class="submenuLink">Shared Folder</a></li>
+					</ul>
+				</li>
+				<li>|</li>
+				<li class="topMenuLi">
+					<a  class="menuLink">Community</a>
+					<ul class="submenu">
+						<li><a href="community.php?field=0" class="submenuLink">Anonymous</a></li>
+						<li><a href="community.php?field=1" class="submenuLink">Suggestion</a></li>
+						<li><a href="community.php?field=2" class="submenuLink">Class Tip</a></li>
+						<li><a href="community.php?field=3" class="submenuLink">Recruitment</a></li>
+					</ul>
+				</li>
+				<li>|</li>
+				<li class="topMenuLi">
+					<a href="gallery.html" class="menuLink">Gallery</a>
+				</li>
+			</ul>
+		</nav>
+		<?php $field_num = $_GET["field"];
+
+		$field_name;
+
+		if($field_num == 0){
+			$field_name = "Anonymous";
+		} else if($field_num == 1){
+			$field_name ="Suggestion";
+		} else if($field_num == 2){
+			$field_name ="Class Tip";
+		} else if($field_num == 3){
+			$field_name ="Recruitment";
+		}
+
+		?>
 
 			<div class="topMain">
 				<ul>
-					<p>
-						<?php
-						if($field_num == 0){
-							print "Anonymous";
-						} else if($field_num == 1){
-							print "Suggestion";
-						} else if($field_num == 2){
-							print "Class Tip";
-						} else if($field_num == 3){
-							print "Recruitment";
-						}
-						?>
-					</p>
+					<p><?=$field_name?></p>
 				</ul>
 			</div>
 		</div>
@@ -142,7 +146,7 @@
 	?>
 
 	<div class="community">
-		<table>
+		<table id="community_table">
 			<tr>
 				<th>글번호</th>
 				<?php if($field_num !== 0){ ?>
@@ -157,12 +161,12 @@
 			<?php
 				for($i=0; $i<$size; $i++){
 					?>
-					<tr>
+					<tr >
 						<td><?=$i+1?></td>
 						<?php if($field_num !== 0){ ?>
 							<td><?=$post_writer[$i]?></td>
 						<?php } ?>
-						<td><?=$post_title[$i]?></td>
+						<td onclick="javascript:ShowPost(this);"><?=$post_title[$i]?></td>
 						<td><?=$post_date[$i]?></td>
 						<td><?=$post_time[$i]?></td>
 						<td><?=$post_hits[$i]?></td>
@@ -171,6 +175,20 @@
 				}
 			?>
 		</table>
+	</div>
+
+	<div class="posting">
+		<fieldset>
+			<select name="post_field">
+				<option>익명게시판</option>
+				<option>건의사항</option>
+				<option>강의자료 및 팁</option>
+				<option>프로젝트 구인</option>
+			</select><br />
+			<label>작성자 이름</label><input type="text" name="writer"><br />
+			<label>제목</label><input type="text" name="title"><br />
+			<textarea name="post" rows="8" cols="80">여기에 글을 입력하세요.</textarea>
+		</fieldset>
 	</div>
 
 </body>
